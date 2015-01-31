4bc1deabf15fcbb2ff7919ec246a5d0f8034a767
Alex Hesselgrave <ahesselgrave@ucla.edu>
2015-01-31 00:09:01 -0800

    Modified main activity to use right XML file

--- app/src/main/java/jycprogrammer/ultimatedbz/ezlapse/LapseGridActivity.java
+++ app/src/main/java/jycprogrammer/ultimatedbz/ezlapse/LapseGridActivity.java
@@ -11,7 +11,7 @@ public class LapseGridActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lapse_grid);
+        setContentView(R.layout.activity_no_lapse);
     }
 
 
